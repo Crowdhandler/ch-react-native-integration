301257e651bf2abcc8ed664411341e61481561d2
cwells-mm <carl.wells@crowdhandler.com>
2023-05-23 12:57:10 +0100

    Updated provider to pass timeout settings to gatekeeper

--- CrowdHandlerProvider.tsx
+++ CrowdHandlerProvider.tsx
@@ -4,9 +4,9 @@ import CHReactNativeGatekeeper from './CHReactNativeGatekeeper';
 
 const CrowdHandlerContext = createContext(null)
 
-export const CrowdHandlerProvider = ({apiKey, chEvents, children, debug=false}) => {
+export const CrowdHandlerProvider = ({apiKey, chEvents, children, timeout, debug=false}) => {
   const CH_CONFIG = { CH_KEY : apiKey }
-  const gatekeeper = new CHReactNativeGatekeeper(CH_CONFIG, chEvents, debug);
+  const gatekeeper = new CHReactNativeGatekeeper(CH_CONFIG, chEvents, timeout, debug);
   return <CrowdHandlerContext.Provider value={gatekeeper}>{children}</CrowdHandlerContext.Provider>
 }
  
